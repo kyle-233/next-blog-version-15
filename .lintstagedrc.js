457e7519a0c51f1c34177295ba2d62054ddd03d9
kyle-233 <kyle07142113@163.com>
2025-03-30 02:45:05 +0800

    feat: initialize the project

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+/**
+ * @filename: .lintstagedrc.js
+ * @type {import('lint-staged').Configuration}
+ */
+import path from 'path'
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+export default {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx,md,html,css}': 'prettier --write',
+}
