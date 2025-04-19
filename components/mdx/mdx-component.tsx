0c0a0778a9ae0876f4bc44b7b76c1518bedba956
kyle-233 <kyle07142113@163.com>
2025-04-19 20:45:53 +0800

    feat: add some components

--- components/mdx/mdx-component.tsx
+++ components/mdx/mdx-component.tsx
@@ -1,9 +1,9 @@
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
 import rehypePrettyCode from 'rehype-pretty-code'
-// import { MdxImage } from './mdx-image'
-// import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils'
+import { MdxImage } from './mdx-image'
+import { MdxCode } from './mdx-code'
 import '@/styles/mdx.css'
-// import { MdxCode } from './mdx-code'
 
 interface MdxProps {
   content: string
@@ -13,69 +13,61 @@ interface MdxProps {
 export const Mdx = async ({ content = '', postComponents = {} }: MdxProps) => {
   const components = {
     ...postComponents,
-    // Counter: Counter,
-    // Image: MdxImage,
-    // pre: MdxCode,
-    // table: ({
-    //   className,
-    //   ...props
-    // }: React.HTMLAttributes<HTMLTableElement>) => (
-    //   <div className="my-6 w-full overflow-y-auto">
-    //     <table className={cn('w-full', className)} {...props} />
-    //   </div>
-    // ),
-    // tr: ({
-    //   className,
-    //   ...props
-    // }: React.HTMLAttributes<HTMLTableRowElement>) => (
-    //   <tr
-    //     className={cn('m-0 border-t p-0 even:bg-muted', className)}
-    //     {...props}
-    //   />
-    // ),
-    // th: ({
-    //   className,
-    //   ...props
-    // }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    //   <th
-    //     className={cn(
-    //       'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
-    //       className,
-    //     )}
-    //     {...props}
-    //   />
-    // ),
-    // td: ({
-    //   className,
-    //   ...props
-    // }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    //   <td
-    //     className={cn(
-    //       'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
-    //       className,
-    //     )}
-    //     {...props}
-    //   />
-    // ),
-    // button: ({
-    //   className,
-    //   ...props
-    // }: React.HTMLAttributes<HTMLButtonElement>) => (
-    //   <button className={cn('', className)} {...props} />
-    // ),
+    Image: MdxImage,
+    pre: MdxCode,
+    table: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableElement>) => (
+      <div className="my-6 w-full overflow-y-auto">
+        <table className={cn('w-full', className)} {...props} />
+      </div>
+    ),
+    tr: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableRowElement>) => (
+      <tr
+        className={cn('m-0 border-t p-0 even:bg-muted', className)}
+        {...props}
+      />
+    ),
+    th: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableCellElement>) => (
+      <th
+        className={cn(
+          'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
+          className,
+        )}
+        {...props}
+      />
+    ),
+    td: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableCellElement>) => (
+      <td
+        className={cn(
+          'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
+          className,
+        )}
+        {...props}
+      />
+    ),
+    button: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLButtonElement>) => (
+      <button className={cn('', className)} {...props} />
+    ),
   }
 
   const options: MDXRemoteProps['options'] = {
-    // useDynamicImport: true,
     parseFrontmatter: true,
     mdxOptions: {
-      // remarkPlugins: [
-      //   remarkGfm,
-      //   // [remarkToc, { maxDepth: 4 }],
-      //   remarkMath,
-      // ],
       rehypePlugins: [
-        // rehypeKatex,
         [
           rehypePrettyCode,
           {
